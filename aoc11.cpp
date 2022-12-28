d5e30e7b63bbd48ec7b11fec4fe9662cdc3b0de5
Mark Dixon <1756429+mnd999@users.noreply.github.com>
2022-12-28 17:20:33 +0000

    format

--- aoc11.cpp
+++ aoc11.cpp
@@ -9,135 +9,141 @@
 using namespace std;
 
 class Monkey {
-  public:
-    list<mpz_class> items;
-    string opleft;
-    string op;
-    string opright;
-    int testDivisor;
-    int isTrue;
-    int isFalse;
-    int count = 0;
+public:
+  list<mpz_class> items;
+  string opleft;
+  string op;
+  string opright;
+  int testDivisor;
+  int isTrue;
+  int isFalse;
+  int count = 0;
 };
 
-vector<Monkey*> parse() {
+vector<Monkey *> parse() {
   std::string line;
-  vector<Monkey*> monkeys;
-  
-
+  vector<Monkey *> monkeys;
 
   do {
-  Monkey *m = new Monkey(); 
-  monkeys.push_back(m);
-  
-  string s;
-  getline(std::cin, line);
-  istringstream iss(line);
-  iss >> s >> s;
+    Monkey *m = new Monkey();
+    monkeys.push_back(m);
 
-  getline(std::cin, line);
-  istringstream iss2(line);
-  iss2 >> s >> s;  
+    string s;
+    getline(std::cin, line);
+    istringstream iss(line);
+    iss >> s >> s;
 
-  string itm;
-  while (iss2 >> itm) {
-    if (itm.at(itm.size()-1) == ',') itm.pop_back();
-    m->items.push_back(stoi(itm));
-  }
-  
-  getline(std::cin, line);
-  istringstream iss3(line);
-  iss3 >> s >> s >> s >> m->opleft >> m->op >> m->opright;
+    getline(std::cin, line);
+    istringstream iss2(line);
+    iss2 >> s >> s;
 
-//  cout << m->opleft << " " << m->op << " " << m->opright << "\n";
+    string itm;
+    while (iss2 >> itm) {
+      if (itm.at(itm.size() - 1) == ',')
+        itm.pop_back();
+      m->items.push_back(stoi(itm));
+    }
 
+    getline(std::cin, line);
+    istringstream iss3(line);
+    iss3 >> s >> s >> s >> m->opleft >> m->op >> m->opright;
 
-  getline(std::cin, line);
-  istringstream iss4(line);
-  iss4 >> s >> s >> s >> m->testDivisor;
+    //  cout << m->opleft << " " << m->op << " " << m->opright << "\n";
 
-  getline(std::cin, line);
-  istringstream iss5(line);
-  iss5 >> s >> s >> s >> s >> s >> m->isTrue;
+    getline(std::cin, line);
+    istringstream iss4(line);
+    iss4 >> s >> s >> s >> m->testDivisor;
 
-  getline(std::cin, line);
-  istringstream iss6(line);
-  iss6 >> s >> s >> s >> s >> s >> m->isFalse;
+    getline(std::cin, line);
+    istringstream iss5(line);
+    iss5 >> s >> s >> s >> s >> s >> m->isTrue;
+
+    getline(std::cin, line);
+    istringstream iss6(line);
+    iss6 >> s >> s >> s >> s >> s >> m->isFalse;
 
   } while (getline(cin, line));
   return monkeys;
 }
 
-
 int main() {
 
- vector<Monkey*> monkeys = parse();
+  vector<Monkey *> monkeys = parse();
 
-
- for (vector<Monkey *>::iterator it = monkeys.begin(); it != monkeys.end(); ++it) {
-  Monkey *m = *it; 
-   for (list<mpz_class>::iterator iit = m->items.begin(); iit != m->items.end(); ++iit) {
-  cout << *iit << ",";
-}
-cout <<"\n";
-}
-
-
-cout << "\n";
-
-for (int i = 0; i<10000; i++) {
-if (i % 100 == 0) cout << i << "\n"; 
- for (vector<Monkey *>::iterator it = monkeys.begin(); it != monkeys.end(); ++it) {
-  Monkey *m = *it; 
-   for (list<mpz_class>::iterator iit = m->items.begin(); iit != m->items.end(); ) {
-      //cout << *iit << ":";
-      m->count++;
-      mpz_class left = 0;
-      if (m->opleft == "old") left = *iit;
-      else left = stoi(m->opleft);
-      mpz_class right = 0;
-      if (m->opright == "old") right = *iit;
-      else right = stoi(m->opright);
-      
-      //cout << left << m->op << right;
-
-      mpz_class newworry = 0;
-      if (m->op == "+") newworry = left + right;
-      if (m->op == "*") newworry = left * right;
-
-      //newworry = newworry / d;
-
-      if (newworry % m->testDivisor == 0) {
-        monkeys.at(m->isTrue)->items.push_back(newworry);
-      } else {
-        monkeys.at(m->isFalse)->items.push_back(newworry);
-      }
-      iit = m->items.erase(iit);
-
+  for (vector<Monkey *>::iterator it = monkeys.begin(); it != monkeys.end();
+       ++it) {
+    Monkey *m = *it;
+    for (list<mpz_class>::iterator iit = m->items.begin();
+         iit != m->items.end(); ++iit) {
+      cout << *iit << ",";
     }
-    //cout << "\n";
- }
+    cout << "\n";
+  }
 
-  //cout << "\n";
-}
- 
-vector<long> counts(monkeys.size(), 0);
- for (vector<Monkey *>::iterator it = monkeys.begin(); it != monkeys.end(); ++it) {
-  Monkey *m = *it; 
-   for (list<mpz_class>::iterator iit = m->items.begin(); iit != m->items.end(); ++iit) {
-  //cout << *iit << ",";
-}
-//cout << "\n" << "count: " << m->count <<"\n";
-counts.push_back(m->count);
-}
+  cout << "\n";
 
-sort(counts.begin(), counts.end());
-vector<long>::iterator it = counts.end();
-it--;
-long c1 = *it;
-it --;
-long c2 = *it;
-long mb = c1 * c2;
-cout << c1 << " " << c2 << " " << mb << "\n";
-}
+  for (int i = 0; i < 10000; i++) {
+    if (i % 100 == 0)
+      cout << i << "\n";
+    for (vector<Monkey *>::iterator it = monkeys.begin(); it != monkeys.end();
+         ++it) {
+      Monkey *m = *it;
+      for (list<mpz_class>::iterator iit = m->items.begin();
+           iit != m->items.end();) {
+        // cout << *iit << ":";
+        m->count++;
+        mpz_class left = 0;
+        if (m->opleft == "old")
+          left = *iit;
+        else
+          left = stoi(m->opleft);
+        mpz_class right = 0;
+        if (m->opright == "old")
+          right = *iit;
+        else
+          right = stoi(m->opright);
 
+        // cout << left << m->op << right;
+
+        mpz_class newworry = 0;
+        if (m->op == "+")
+          newworry = left + right;
+        if (m->op == "*")
+          newworry = left * right;
+
+        // newworry = newworry / d;
+
+        if (newworry % m->testDivisor == 0) {
+          monkeys.at(m->isTrue)->items.push_back(newworry);
+        } else {
+          monkeys.at(m->isFalse)->items.push_back(newworry);
+        }
+        iit = m->items.erase(iit);
+      }
+      // cout << "\n";
+    }
+
+    // cout << "\n";
+  }
+
+  vector<long> counts(monkeys.size(), 0);
+  for (vector<Monkey *>::iterator it = monkeys.begin(); it != monkeys.end();
+       ++it) {
+    Monkey *m = *it;
+    for (list<mpz_class>::iterator iit = m->items.begin();
+         iit != m->items.end(); ++iit) {
+      // cout << *iit << ",";
+    }
+    // cout << "\n" << "count: " << m->count <<"\n";
+    counts.push_back(m->count);
+  }
+
+  sort(counts.begin(), counts.end());
+  vector<long>::iterator it = counts.end();
+  it--;
+  long c1 = *it;
+  it--;
+  long c2 = *it;
+  long mb = c1 * c2;
+  cout << c1 << " " << c2 << " " << mb << "\n";
+}
--- aoc22.cpp
+++ aoc22.cpp
@@ -21,8 +21,10 @@ int main() {
   int x = 0;
   int y = 0;
   int dir = 0;
-  for(string::iterator it = map.begin()->begin(); it != map.begin()->end(); it++) {
-    if (*it == '.') break;
+  for (string::iterator it = map.begin()->begin(); it != map.begin()->end();
+       it++) {
+    if (*it == '.')
+      break;
     x++;
   }
 
@@ -32,9 +34,9 @@ int main() {
   std::getline(std::cin, directions);
   string::iterator it = directions.begin();
   string tmp = "";
-  while(it != directions.end()) {
-    while (*it >= '0' && *it <='9' && it != directions.end()) {
-      tmp.push_back(*it); 
+  while (it != directions.end()) {
+    while (*it >= '0' && *it <= '9' && it != directions.end()) {
+      tmp.push_back(*it);
       it++;
     }
     if (tmp.size() > 0) {
@@ -43,88 +45,115 @@ int main() {
       tmp = "";
 
       string row = map.at(y);
-      switch(dir) {
-        case 0: 
-          for (int i = 0; i< dist; i++) {
-            //cout << row << x << "\n";
-            if (x+1 < row.length()) {
-                char n = row.at(x+1);
-                if (n == '.' || n == '-') x++;
-                else if (n == '#') break;
-                else exit(-1);
-            } else {
-               int np = 0;
-               //cout << np << ": " << row.at(np);
-               while (row.at(np) == ' ') np++;
-               if (row.at(np) == '#')  break;
-               else x = np;
-            }
+      switch (dir) {
+      case 0:
+        for (int i = 0; i < dist; i++) {
+          // cout << row << x << "\n";
+          if (x + 1 < row.length()) {
+            char n = row.at(x + 1);
+            if (n == '.' || n == '-')
+              x++;
+            else if (n == '#')
+              break;
+            else
+              exit(-1);
+          } else {
+            int np = 0;
+            // cout << np << ": " << row.at(np);
+            while (row.at(np) == ' ')
+              np++;
+            if (row.at(np) == '#')
+              break;
+            else
+              x = np;
           }
-          break;
-        case 1:
-          for (int i = 0; i< dist; i++) {
-            if (y < map.size() -1 && map.at(y+1).size() > x  && map.at(y+1).at(x) != ' ')   {
-              char n = map.at(y+1).at(x);
-              if (n == '.' || n == '-') y++;
-              else if (n == '#') break;
-              else exit(-1);
-            } else {
-               int np = 0;
-               while (map.at(np).size() < x || map.at(np).at(x) == ' ') np++;
-               if (map.at(np).at(x) == '#')  break;
-               else y = np;
-            }
+        }
+        break;
+      case 1:
+        for (int i = 0; i < dist; i++) {
+          if (y < map.size() - 1 && map.at(y + 1).size() > x &&
+              map.at(y + 1).at(x) != ' ') {
+            char n = map.at(y + 1).at(x);
+            if (n == '.' || n == '-')
+              y++;
+            else if (n == '#')
+              break;
+            else
+              exit(-1);
+          } else {
+            int np = 0;
+            while (map.at(np).size() < x || map.at(np).at(x) == ' ')
+              np++;
+            if (map.at(np).at(x) == '#')
+              break;
+            else
+              y = np;
           }
-          break;
-        case 2: 
-          for (int i = 0; i< dist; i++) {
-            if (x != 0 && row.at(x-1) != ' ') {
-              char n = row.at(x-1);
-              if (n == '.' || n == '-') x--;
-              else if (n == '#') break;
-              else exit(-1);
-            } else {
-               int np = row.size() - 1;
-               while (row.at(np) == ' ') np--;
-               if (row.at(np) == '#')  break;
-               else x = np;
-            }
+        }
+        break;
+      case 2:
+        for (int i = 0; i < dist; i++) {
+          if (x != 0 && row.at(x - 1) != ' ') {
+            char n = row.at(x - 1);
+            if (n == '.' || n == '-')
+              x--;
+            else if (n == '#')
+              break;
+            else
+              exit(-1);
+          } else {
+            int np = row.size() - 1;
+            while (row.at(np) == ' ')
+              np--;
+            if (row.at(np) == '#')
+              break;
+            else
+              x = np;
           }
-          break;
-        case 3:
-          for (int i = 0; i< dist; i++) {
-            if (y != 0 && map.at(y-1).size() > x && map.at(y-1).at(x) != ' ') {
-              char n = map.at(y-1).at(x);
-              if (n == '.' || n == '-') y--;
-              else if (n == '#') break;
-              else exit(-1);
-            } else {
-               int np = map.size() - 1; 
-               while (map.at(np).size() <= x || map.at(np).at(x) == ' ') np--;
-               if (map.at(np).at(x) == '#')  break;
-               else y = np;
-            }
+        }
+        break;
+      case 3:
+        for (int i = 0; i < dist; i++) {
+          if (y != 0 && map.at(y - 1).size() > x &&
+              map.at(y - 1).at(x) != ' ') {
+            char n = map.at(y - 1).at(x);
+            if (n == '.' || n == '-')
+              y--;
+            else if (n == '#')
+              break;
+            else
+              exit(-1);
+          } else {
+            int np = map.size() - 1;
+            while (map.at(np).size() <= x || map.at(np).at(x) == ' ')
+              np--;
+            if (map.at(np).at(x) == '#')
+              break;
+            else
+              y = np;
           }
-          break;
+        }
+        break;
       }
 
       cout << x << "," << y << ":" << dir << "\n";
     }
-    if (*it == 'R') dir = (dir + 1) % 4;
+    if (*it == 'R')
+      dir = (dir + 1) % 4;
     if (*it == 'L') {
       dir--;
-      if (dir < 0) dir = 3;
+      if (dir < 0)
+        dir = 3;
     }
-    if (it != directions.end()) it++;
-    if (dir < 0 || dir > 3) exit(-1);
+    if (it != directions.end())
+      it++;
+    if (dir < 0 || dir > 3)
+      exit(-1);
   }
 
-  cout << 1000 * (y+1) + 4 * (x+1) + dir << "\n";
+  cout << 1000 * (y + 1) + 4 * (x + 1) + dir << "\n";
 
   for (vector<string>::iterator it = map.begin(); it != map.end(); ++it) {
-//    cout << *it << "\n";
+    //    cout << *it << "\n";
   }
-
-
 }
-
