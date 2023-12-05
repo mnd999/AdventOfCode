b162c6c7b3eccb6381667ab78235d52da5848dfa
Mark Dixon <1756429+mnd999@users.noreply.github.com>
2023-12-05 17:23:11 +0000

    Fix round 2

--- 2023/aoc2.hs
+++ 2023/aoc2.hs
@@ -2,23 +2,51 @@ module Main where
 
 import Data.Char
 
-data Game = Game { id :: Int, picks :: [Pick] }
-data Pick = Pick { red :: Int, green :: Int, blue :: Int }
+data Game = Game { id :: Int, picks :: [Pick] } deriving Show
+data Pick = Pick { red :: Int, green :: Int, blue :: Int } deriving Show
 
-instance Read Game where
-  readsPrec _ ('G':'a':'m':'e':' ':n:':':therest) = 
-		[(Game (digitToInt n) [], therest)]
+test :: String
+test = "Game 99: 15 red, 7 green, 11 blue; 2 blue, 12 green, 17 red; 6 red, 3 blue, 11 green; 14 red, 13 green, 5 blue"
 
-instance Read Pick where
-	readsPrec _ input = [(Pick 1 2 3, "")]
-				
-instance Show Game where
-	show (Game id picks) = show id
+parse :: String -> Game
+parse s = case break (==':') s of
+            (g, p:ps) -> Game (parseGame g) (parsePicks ps)
 
-readGame :: IO Game
-readGame = readLn
+parseGame :: [Char] -> Int
+parseGame cs = read (dropWhile (\x -> not (isDigit x)) cs) :: Int
+
+parsePicks :: [Char] -> [Pick]
+parsePicks cs = map parsePick (splitOn ';' cs) 
+
+parsePick :: [Char] -> Pick
+parsePick cs = foldr parsePick' (Pick 0 0 0) (splitOn ',' cs)
+
+
+parsePick' :: String -> Pick -> Pick
+parsePick' cs pick = case words cs of
+                       n:"red":_ -> Pick (read n :: Int) (green pick) (blue pick)
+                       n:"green":_ -> Pick (red pick) (read n :: Int) (blue pick)
+                       n:"blue":_ -> Pick (red pick) (green pick) (read n :: Int)
+                      
+
+-- Why do I have to write this myself?
+splitOn :: Char -> [Char] -> [[Char]]
+splitOn c [] = []
+splitOn c cs = case break (==c) cs of
+                 (h, "") -> [h]
+                 (h, t:ts) -> [h] ++ (splitOn c ts)
+
+filterGames :: [Game] -> [Game]
+filterGames gs = filter (\g -> all pickIsValid (picks g)) gs
+
+pickIsValid :: Pick -> Bool
+pickIsValid p = (red p) <= 12 && (green p) <= 13 && (blue p) <= 14
 
 main :: IO ()
 
-main = readGame map show
-
+main = do 
+  lines <- getContents
+  let games = map parse (splitOn '\n' lines)
+  let fgames = filterGames games
+  let total = sum (map Main.id fgames)
+  print total
