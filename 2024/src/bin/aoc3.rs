7c1604be494b461942099f4ab8673dc163405a14
Mark Dixon <1756429+mnd999@users.noreply.github.com>
2024-12-03 13:19:02 +0000

    Finish day3

--- 2024/src/bin/aoc3.rs
+++ 2024/src/bin/aoc3.rs
@@ -18,7 +18,31 @@ fn main() -> io::Result<()> {
 
         let sum = vals.iter().fold(0, |acc, x| acc + x);
 
-        print!("{:?}, {}", vals, sum);
+        print!("{:?}, {}\n", vals, sum);
+
+        let re2 = Regex::new(r"(mul|do|don't)(\(([0-9]+),([0-9]+)\)|\(()()\))").unwrap();
+
+        let vals2 : Vec<(&str, &str, &str)> = re2.captures_iter(&buffer).map(|caps| {
+            let (_, [cmd, _, x, y]) = caps.extract();
+            (cmd, x,y)
+        }).collect();
+
+        let mut total = 0;
+        let mut state = 1;
+        for (cmd, x, y) in vals2 {
+            if cmd == "don't" {
+                state = 0;
+            }
+            if cmd == "do" {
+                state = 1;
+            }
+            if cmd == "mul" && state == 1 {
+                total = total + x.parse::<i32>().unwrap() * y.parse::<i32>().unwrap()
+            }
+        }        
+
+        print!("Total: {}\n", total);
+
     }
 
 
