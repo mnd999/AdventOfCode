922be61b0195d28305386564cf61e7728795206a
Mark Dixon <1756429+mnd999@users.noreply.github.com>
2025-01-02 11:27:59 +0000

    Day24 part1

---
+++ 2024/src/bin/aoc24.rs
@@ -0,0 +1,92 @@
+extern crate regex;
+
+use std::collections::HashMap;
+use std::collections::VecDeque;
+use std::io;
+use regex::Regex;
+
+
+#[derive(Debug)]
+enum Op {
+    XOR,
+    OR,
+    AND,
+    UNDEFN
+}
+
+fn main() -> io::Result<()> {
+    let lines = io::stdin().lines();
+    let mut vars: HashMap<String, i32> = HashMap::new();
+    let mut fns: VecDeque<(String, Op, String, String)> = VecDeque::new();
+
+    let re1 = Regex::new(r"^([a-z][0-9][0-9]): ([01])").unwrap();
+    let re2 = Regex::new(r"^([a-z0-9]+) ([A-Z]+) ([a-z0-9]+) -> ([a-z0-9]+)").unwrap();
+
+    for line in lines {
+        let l = line.unwrap();
+        println!("{}", &l);
+
+        re1.captures(&l).map( |cap| vars.insert(cap[1].to_string(), cap[2].parse::<i32>().unwrap()));
+
+        re2.captures(&l).map( |cap| fns.push_back((cap[1].to_string(), to_op(&cap[2]), cap[3].to_string(), cap[4].to_string())));
+
+    }
+    println!("{:?}", vars);
+    println!("{:?}", fns);
+
+    while !fns.is_empty() {
+        let (x, o, y, r) = fns.pop_front().unwrap();
+        if vars.contains_key(&x) && vars.contains_key(&y) {
+            let vx = vars[&x];
+            let vy = vars[&y];
+
+            match o {
+                Op::OR => {
+                    if vx == 1 || vy == 1 { 
+                        vars.insert(r, 1);
+                    } else {
+                        vars.insert(r,0);
+                    }
+                },
+                Op::XOR => {
+                    let t = vx + vy;
+                    if t == 1 {
+                        vars.insert(r,1);
+                    } else {
+                        vars.insert(r,0);
+                    }
+                }
+                Op::AND => {
+                    if vx == 1 && vy == 1 {
+                        vars.insert(r,1);
+                    } else {
+                        vars.insert(r,0);
+                    }
+                }
+                Op::UNDEFN => {}
+            }
+        } else {
+            fns.push_back((x,o,y,r));
+        }
+    }
+
+
+     let mut zs: Vec<(&String, &i32)> = vars.iter().filter(|(k,_)| k.starts_with("z")).collect::<Vec<(&String, &i32)>>();
+     zs.sort_by(|(k,_), (k1, _)| k.cmp(k1));
+     zs.reverse();
+
+     println!("Zs: {:?}", zs);
+
+     zs.iter().for_each(|(_,v)| print!("{}", v));
+
+    Ok(())
+}
+
+fn to_op(s: &str) -> Op {
+    return match s {
+        "XOR" => Op::XOR,
+        "OR" => Op::OR,
+        "AND" => Op::AND,
+        _ => Op::UNDEFN,
+    }
+}
